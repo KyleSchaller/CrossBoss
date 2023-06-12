b90ce5c0cff9135419537c3d109af7af4d467b09
Kyle <schaller.k@gmail.com>
2023-06-11 17:58:44 -0700

    Fixed indexing error, added MC

---
+++ plot_anthill.m
@@ -0,0 +1,58 @@
+clear all
+close all
+clc
+
+B = dir;
+
+xtotf = [];
+ytotf = [];
+ztotf = [];
+
+for i = 3:length(B)
+    load(B(i).name)
+    xtotf = [xtotf; Data.x_tot];
+    ytotf = [ytotf; Data.y_tot];
+    ztotf = [ztotf; Data.z_tot];
+
+    % ytotym = Data.y_tot;
+    % ytotym = ytotym - 5;
+    % ytotym = -1*ytotym;
+    % ytotym = ytotym + 5;
+    % 
+    % xtotf = [xtotf; Data.x_tot];
+    % ytotf = [ytotf; ytotym];
+    % ztotf = [ztotf; Data.z_tot];
+end    
+
+indx = ztotf>-0;
+ztotf(indx) = nan;
+xtotf(indx) = nan;
+ytotf(indx) = nan;
+
+ztotf = ztotf(~isnan(ztotf));
+ytotf = ytotf(~isnan(ytotf));
+xtotf = xtotf(~isnan(xtotf));
+
+figure
+ax=gca
+ax.Color = [.5,.5,.5];
+% ax.DataAspectRatio = [1,1,1];
+hold on
+
+scatter3(xtotf,ytotf,ztotf,'MarkerEdgeColor','k','SizeData',5)
+
+xgrid = 0:.1:10;
+ygrid = 0:.1:10;
+
+for i = 1:length(xgrid)-1
+    for j = 1:length(ygrid)-1
+        indx_t = xtotf >= xgrid(i) & xtotf < xgrid(i+1) & ytotf >= ygrid(j) & ytotf < ygrid(j+1);
+        value_grid(i,j) = sum(indx_t);
+    end
+end
+
+value_grid(value_grid>1000) = 1000;
+figure
+contourf(xgrid(1:end-1),ygrid(1:end-1),value_grid')
+% caxis([1000,100000])
+colormap('jet')
